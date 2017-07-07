595a3ab7171814db5a0e282cf88a410f6e95b746
dyshero <herodys@163.com>
2017-07-07 15:06:25 +0800

    commit init

---
+++ MVVMDemo/MVVMDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MVVMDemo
+//
+//  Created by duodian on 2017/6/13.
+//  Copyright © 2017年 duodian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
